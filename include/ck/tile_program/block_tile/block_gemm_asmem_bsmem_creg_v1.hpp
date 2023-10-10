76985f718d14150aa3cd6e98b62b5757a7f25c50
Chao Liu <chao.liu2@amd.com>
2023-10-10 11:18:14 -0400

    refactor

--- include/ck/tile_program/block_tile/block_gemm_asmem_bsmem_creg_v1.hpp
+++ include/ck/tile_program/block_tile/block_gemm_asmem_bsmem_creg_v1.hpp
@@ -49,6 +49,7 @@ struct BlockGemmASmemBSmemCRegV1
 
     static constexpr index_t kBlockSize = Problem::kBlockSize;
 
+#if 0
     // C += A * B
     template <typename CBlockTensor, typename ABlockWindowTmp, typename BBlockWindowTmp>
     __device__ void operator()(CBlockTensor& c_block_tensor,
@@ -168,7 +169,7 @@ struct BlockGemmASmemBSmemCRegV1
                     // read C warp tensor from C block tensor
                     CWarpTensor c_warp_tensor;
 
-                    c_warp_tensor.GetThreadBuffer() = c_block_tensor.GetSlicedThreadData(
+                    c_warp_tensor.GetThreadBuffer() = c_block_tensor.GetYSlicedThreadData(
                         merge_sequences(Sequence<mIter, nIter>{}, c_warp_y_index_zeros),
                         merge_sequences(Sequence<1, 1>{}, c_warp_y_lengths));
 
@@ -176,7 +177,7 @@ struct BlockGemmASmemBSmemCRegV1
                     WG{}(c_warp_tensor, a_warp_tensor, b_warp_tensor);
 
                     // write C warp tensor into C block tensor
-                    c_block_tensor.SetSlicedThreadData(
+                    c_block_tensor.SetYSlicedThreadData(
                         merge_sequences(Sequence<mIter, nIter>{}, c_warp_y_index_zeros),
                         merge_sequences(Sequence<1, 1>{}, c_warp_y_lengths),
                         c_warp_tensor.GetThreadBuffer());
@@ -184,7 +185,158 @@ struct BlockGemmASmemBSmemCRegV1
             });
         });
     }
+#else
+    // C += A * B
+    template <typename CBlockTensor, typename ABlockWindowTmp, typename BBlockWindowTmp>
+    __device__ void operator()(CBlockTensor& c_block_tensor,
+                               const ABlockWindowTmp& a_block_window_tmp,
+                               const BBlockWindowTmp& b_block_window_tmp) const
+    {
+        static_assert(is_same_v<ADataType, typename ABlockWindowTmp::DataType> &&
+                          is_same_v<BDataType, typename BBlockWindowTmp::DataType> &&
+                          is_same_v<CDataType, typename CBlockTensor::DataType>,
+                      "wrong!");
 
+        constexpr index_t MPerBlock = ABlockWindowTmp{}.GetWindowLengths()[Number<0>{}];
+        constexpr index_t NPerBlock = BBlockWindowTmp{}.GetWindowLengths()[Number<0>{}];
+        constexpr index_t KPerBlock = ABlockWindowTmp{}.GetWindowLengths()[Number<1>{}];
+
+        static_assert(MPerBlock == BlockGemmShape::kM && NPerBlock == BlockGemmShape::kN &&
+                          KPerBlock == BlockGemmShape::kK,
+                      "wrong!");
+
+        constexpr auto config = Policy::template GetWarpGemmMWarpNWarp<Problem>();
+
+        using WG = remove_cvref_t<decltype(config.template At<0>())>;
+
+        constexpr index_t MWarp = config.template At<1>();
+        constexpr index_t NWarp = config.template At<2>();
+
+        constexpr index_t MIterPerWarp = MPerBlock / (MWarp * WG::kM);
+        constexpr index_t NIterPerWarp = NPerBlock / (NWarp * WG::kN);
+        constexpr index_t KIterPerWarp = KPerBlock / WG::kK;
+
+        constexpr index_t MPerBlockPerIter = MPerBlock / MIterPerWarp;
+        constexpr index_t NPerBlockPerIter = NPerBlock / NIterPerWarp;
+        constexpr index_t KPerBlockPerIter = KPerBlock / KIterPerWarp;
+
+        constexpr auto a_block_outer_dstr_encoding =
+            StaticTileDistributionEncoding<Sequence<NWarp>,
+                                           Tuple<Sequence<1, MWarp>, Sequence<1>>,
+                                           Tuple<Sequence<1, 0>>,
+                                           Tuple<Sequence<1, 0>>,
+                                           Sequence<1, 2>,
+                                           Sequence<0, 0>>{};
+
+        constexpr auto b_block_outer_dstr_encoding =
+            StaticTileDistributionEncoding<Sequence<MWarp>,
+                                           Tuple<Sequence<1, NWarp>, Sequence<1>>,
+                                           Tuple<Sequence<0, 1>>,
+                                           Tuple<Sequence<0, 1>>,
+                                           Sequence<1, 2>,
+                                           Sequence<0, 0>>{};
+
+        constexpr auto a_block_dstr_encode = detail::make_embed_tile_distribution_encoding(
+            a_block_outer_dstr_encoding, typename WG::AWarpDstrEncoding{});
+
+        constexpr auto b_block_dstr_encode = detail::make_embed_tile_distribution_encoding(
+            b_block_outer_dstr_encoding, typename WG::BWarpDstrEncoding{});
+
+        constexpr auto a_block_dstr = make_static_tile_distribution(a_block_dstr_encode);
+        constexpr auto b_block_dstr = make_static_tile_distribution(b_block_dstr_encode);
+
+        // construct A-block-window
+        using ABlockWindow = decltype(
+            make_tile_window(a_block_window_tmp.GetBottomTensorView(),
+                             make_tuple(Number<MPerBlockPerIter>{}, Number<KPerBlockPerIter>{}),
+                             a_block_window_tmp.GetWindowOrigin(),
+                             a_block_dstr));
+
+        Array<Array<ABlockWindow, KIterPerWarp>, MIterPerWarp> a_block_windows{{
+            make_tile_window(a_block_window_tmp.GetBottomTensorView(),
+                             make_tuple(Number<MPerBlockPerIter>{}, Number<KPerBlockPerIter>{}),
+                             a_block_window_tmp.GetWindowOrigin(),
+                             a_block_dstr)}};
+
+        for(index_t mIter = 0; mIter < MIterPerWarp; mIter++)
+        {
+            for(index_t kIter = 0; kIter < KIterPerWarp; kIter++)
+            {
+                move_tile_window(a_block_windows(mIter)(kIter),
+                                 {mIter * MPerBlockPerIter, kIter * KPerBlockPerIter});
+            }
+        }
+
+        // construct B-block-window
+        using BBlockWindow = decltype(
+            make_tile_window(b_block_window_tmp.GetBottomTensorView(),
+                             make_tuple(Number<NPerBlockPerIter>{}, Number<KPerBlockPerIter>{}),
+                             b_block_window_tmp.GetWindowOrigin(),
+                             b_block_dstr));
+
+        Array<Array<BBlockWindow, KIterPerWarp>, NIterPerWarp> b_block_windows{{
+            make_tile_window(b_block_window_tmp.GetBottomTensorView(),
+                             make_tuple(Number<NPerBlockPerIter>{}, Number<KPerBlockPerIter>{}),
+                             b_block_window_tmp.GetWindowOrigin(),
+                             b_block_dstr)}};
+
+        for(index_t nIter = 0; nIter < NIterPerWarp; nIter++)
+        {
+            for(index_t kIter = 0; kIter < KIterPerWarp; kIter++)
+            {
+                move_tile_window(b_block_windows(nIter)(kIter),
+                                 {nIter * NPerBlockPerIter, kIter * KPerBlockPerIter});
+            }
+        }
+
+        using CWarpDstr = typename WG::CWarpDstr;
+
+        using AWarpTensor = typename WG::AWarpTensor;
+        using BWarpTensor = typename WG::BWarpTensor;
+        using CWarpTensor = typename WG::CWarpTensor;
+
+        constexpr auto c_warp_y_lengths = to_sequence(CWarpDstr{}.GetYs2DDescriptor().GetLengths());
+        constexpr auto c_warp_y_index_zeros = uniform_sequence_gen_t<CWarpDstr::NDimY, 0>{};
+
+        // hot loop:
+        static_for<0, KIterPerWarp, 1>{}([&](auto kIter) {
+            static_for<0, MIterPerWarp, 1>{}([&](auto mIter) {
+                // read A warp tensor from A block window
+                AWarpTensor a_warp_tensor;
+
+                a_warp_tensor.GetThreadBuffer() = detail::load_thread_data_from_tile_window(
+                    a_block_windows(mIter)(kIter));
+
+                static_for<0, NIterPerWarp, 1>{}([&](auto nIter) {
+                    // read B warp tensor from B Block window
+                    BWarpTensor b_warp_tensor;
+
+                    b_warp_tensor.GetThreadBuffer() =
+                        detail::load_thread_data_from_tile_window(
+                            b_block_windows(nIter)(kIter));
+
+                    // read C warp tensor from C block tensor
+                    CWarpTensor c_warp_tensor;
+
+                    c_warp_tensor.GetThreadBuffer() = c_block_tensor.GetYSlicedThreadData(
+                        merge_sequences(Sequence<mIter, nIter>{}, c_warp_y_index_zeros),
+                        merge_sequences(Sequence<1, 1>{}, c_warp_y_lengths));
+
+                    // warp GEMM
+                    WG{}(c_warp_tensor, a_warp_tensor, b_warp_tensor);
+
+                    // write C warp tensor into C block tensor
+                    c_block_tensor.SetYSlicedThreadData(
+                        merge_sequences(Sequence<mIter, nIter>{}, c_warp_y_index_zeros),
+                        merge_sequences(Sequence<1, 1>{}, c_warp_y_lengths),
+                        c_warp_tensor.GetThreadBuffer());
+                });
+            });
+        });
+    }
+#endif
+
+#if 0
     // C = A * B
     template <typename ABlockWindowTmp, typename BBlockWindowTmp>
     __device__ auto operator()(const ABlockWindowTmp& a_block_window_tmp,
@@ -313,7 +465,7 @@ struct BlockGemmASmemBSmemCRegV1
                     else
                     {
                         // c += a * b
-                        c_warp_tensor.GetThreadBuffer() = c_block_tensor.GetSlicedThreadData(
+                        c_warp_tensor.GetThreadBuffer() = c_block_tensor.GetYSlicedThreadData(
                             merge_sequences(Sequence<mIter, nIter>{}, c_warp_y_index_zeros),
                             merge_sequences(Sequence<1, 1>{}, c_warp_y_lengths));
 
@@ -321,7 +473,7 @@ struct BlockGemmASmemBSmemCRegV1
                     }
 
                     // write C warp tensor into C block tensor
-                    c_block_tensor.SetSlicedThreadData(
+                    c_block_tensor.SetYSlicedThreadData(
                         merge_sequences(Sequence<mIter, nIter>{}, c_warp_y_index_zeros),
                         merge_sequences(Sequence<1, 1>{}, c_warp_y_lengths),
                         c_warp_tensor.GetThreadBuffer());
@@ -331,6 +483,182 @@ struct BlockGemmASmemBSmemCRegV1
 
         return c_block_tensor;
     }
+#else
+    // C = A * B
+    template <typename ABlockWindowTmp, typename BBlockWindowTmp>
+    __device__ auto operator()(const ABlockWindowTmp& a_block_window_tmp,
+                               const BBlockWindowTmp& b_block_window_tmp) const
+    {
+        static_assert(is_same_v<ADataType, typename ABlockWindowTmp::DataType> &&
+                          is_same_v<BDataType, typename BBlockWindowTmp::DataType>,
+                      "wrong!");
+
+        constexpr index_t MPerBlock = ABlockWindowTmp{}.GetWindowLengths()[Number<0>{}];
+        constexpr index_t NPerBlock = BBlockWindowTmp{}.GetWindowLengths()[Number<0>{}];
+        constexpr index_t KPerBlock = ABlockWindowTmp{}.GetWindowLengths()[Number<1>{}];
+
+        static_assert(MPerBlock == BlockGemmShape::kM && NPerBlock == BlockGemmShape::kN &&
+                          KPerBlock == BlockGemmShape::kK,
+                      "wrong!");
+
+        constexpr auto config = Policy::template GetWarpGemmMWarpNWarp<Problem>();
+
+        using WG = remove_cvref_t<decltype(config.template At<0>())>;
+
+        constexpr index_t MWarp = config.template At<1>();
+        constexpr index_t NWarp = config.template At<2>();
+
+        constexpr index_t MIterPerWarp = MPerBlock / (MWarp * WG::kM);
+        constexpr index_t NIterPerWarp = NPerBlock / (NWarp * WG::kN);
+        constexpr index_t KIterPerWarp = KPerBlock / WG::kK;
+
+        constexpr index_t MPerBlockPerIter = MPerBlock / MIterPerWarp;
+        constexpr index_t NPerBlockPerIter = NPerBlock / NIterPerWarp;
+        constexpr index_t KPerBlockPerIter = KPerBlock / KIterPerWarp;
+
+        constexpr auto a_block_outer_dstr_encoding = StaticTileDistributionEncoding<
+            Sequence<NWarp>,
+            Tuple<Sequence<1, MWarp>, Sequence<1>>,
+            Tuple<Sequence<1, 0>>,
+            Tuple<Sequence<1, 0>>,
+            Sequence<1, 2>,
+            Sequence<0, 0>>{};
+
+        constexpr auto b_block_outer_dstr_encoding = StaticTileDistributionEncoding<
+            Sequence<MWarp>,
+            Tuple<Sequence<1, NWarp>, Sequence<1>>,
+            Tuple<Sequence<0, 1>>,
+            Tuple<Sequence<0, 1>>,
+            Sequence<1, 2>,
+            Sequence<0, 0>>{};
+
+        constexpr auto c_block_outer_dstr_encoding = StaticTileDistributionEncoding<
+            Sequence<>,
+            Tuple<Sequence<MIterPerWarp, MWarp>, Sequence<NIterPerWarp, NWarp>>,
+            Tuple<Sequence<1, 2>>,
+            Tuple<Sequence<1, 1>>,
+            Sequence<1, 2>,
+            Sequence<0, 0>>{};
+
+        constexpr auto a_block_dstr_encode = detail::make_embed_tile_distribution_encoding(
+            a_block_outer_dstr_encoding, typename WG::AWarpDstrEncoding{});
+
+        constexpr auto b_block_dstr_encode = detail::make_embed_tile_distribution_encoding(
+            b_block_outer_dstr_encoding, typename WG::BWarpDstrEncoding{});
+
+        constexpr auto c_block_dstr_encode = detail::make_embed_tile_distribution_encoding(
+            c_block_outer_dstr_encoding, typename WG::CWarpDstrEncoding{});
+
+        constexpr auto a_block_dstr = make_static_tile_distribution(a_block_dstr_encode);
+        constexpr auto b_block_dstr = make_static_tile_distribution(b_block_dstr_encode);
+        constexpr auto c_block_dstr = make_static_tile_distribution(c_block_dstr_encode);
+
+        // construct A-block-window
+        using ABlockWindow = decltype(
+            make_tile_window(a_block_window_tmp.GetBottomTensorView(),
+                             make_tuple(Number<MPerBlockPerIter>{}, Number<KPerBlockPerIter>{}),
+                             a_block_window_tmp.GetWindowOrigin(),
+                             a_block_dstr));
+
+        Array<Array<ABlockWindow, KIterPerWarp>, MIterPerWarp> a_block_windows{{
+            make_tile_window(a_block_window_tmp.GetBottomTensorView(),
+                             make_tuple(Number<MPerBlockPerIter>{}, Number<KPerBlockPerIter>{}),
+                             a_block_window_tmp.GetWindowOrigin(),
+                             a_block_dstr)}};
+
+        for(index_t mIter = 0; mIter < MIterPerWarp; mIter++)
+        {
+            for(index_t kIter = 0; kIter < KIterPerWarp; kIter++)
+            {
+                move_tile_window(a_block_windows(mIter)(kIter),
+                                 {mIter * MPerBlockPerIter, kIter * KPerBlockPerIter});
+            }
+        }
+
+        // construct B-block-window
+        using BBlockWindow = decltype(
+            make_tile_window(b_block_window_tmp.GetBottomTensorView(),
+                             make_tuple(Number<NPerBlockPerIter>{}, Number<KPerBlockPerIter>{}),
+                             b_block_window_tmp.GetWindowOrigin(),
+                             b_block_dstr));
+
+        Array<Array<BBlockWindow, KIterPerWarp>, NIterPerWarp> b_block_windows{{
+            make_tile_window(b_block_window_tmp.GetBottomTensorView(),
+                             make_tuple(Number<NPerBlockPerIter>{}, Number<KPerBlockPerIter>{}),
+                             b_block_window_tmp.GetWindowOrigin(),
+                             b_block_dstr)}};
+
+        for(index_t nIter = 0; nIter < NIterPerWarp; nIter++)
+        {
+            for(index_t kIter = 0; kIter < KIterPerWarp; kIter++)
+            {
+                move_tile_window(b_block_windows(nIter)(kIter),
+                                 {nIter * NPerBlockPerIter, kIter * KPerBlockPerIter});
+            }
+        }
+
+        static_assert(is_same_v<CDataType, typename WG::CDataType>, "wrong!");
+
+        // Construct C-Block-Tensor
+        auto c_block_tensor = make_static_distributed_tensor<CDataType>(c_block_dstr);
+
+        using CWarpDstr = typename WG::CWarpDstr;
+
+        using AWarpTensor = typename WG::AWarpTensor;
+        using BWarpTensor = typename WG::BWarpTensor;
+        using CWarpTensor = typename WG::CWarpTensor;
+
+        constexpr auto c_warp_y_lengths = to_sequence(CWarpDstr{}.GetYs2DDescriptor().GetLengths());
+        constexpr auto c_warp_y_index_zeros = uniform_sequence_gen_t<CWarpDstr::NDimY, 0>{};
+
+        // hot loop:
+        static_for<0, KIterPerWarp, 1>{}([&](auto kIter) {
+            static_for<0, MIterPerWarp, 1>{}([&](auto mIter) {
+                // read A warp tensor from A block window
+                AWarpTensor a_warp_tensor;
+
+                a_warp_tensor.GetThreadBuffer() = detail::load_thread_data_from_tile_window(
+                    a_block_windows(mIter)(kIter));
+
+                static_for<0, NIterPerWarp, 1>{}([&](auto nIter) {
+                    // read B warp tensor from B Block window
+                    BWarpTensor b_warp_tensor;
+
+                    b_warp_tensor.GetThreadBuffer() =
+                        detail::load_thread_data_from_tile_window(
+                            b_block_windows(nIter)(kIter));
+
+                    // read C warp tensor from C block tensor
+                    CWarpTensor c_warp_tensor;
+
+                    // warp GEMM
+                    if constexpr(KIterPerWarp == 0)
+                    {
+                        // c = a * b
+                        c_warp_tensor = WG{}(a_warp_tensor, b_warp_tensor);
+                    }
+                    else
+                    {
+                        // c += a * b
+                        c_warp_tensor.GetThreadBuffer() = c_block_tensor.GetYSlicedThreadData(
+                            merge_sequences(Sequence<mIter, nIter>{}, c_warp_y_index_zeros),
+                            merge_sequences(Sequence<1, 1>{}, c_warp_y_lengths));
+
+                        WG{}(c_warp_tensor, a_warp_tensor, b_warp_tensor);
+                    }
+
+                    // write C warp tensor into C block tensor
+                    c_block_tensor.SetYSlicedThreadData(
+                        merge_sequences(Sequence<mIter, nIter>{}, c_warp_y_index_zeros),
+                        merge_sequences(Sequence<1, 1>{}, c_warp_y_lengths),
+                        c_warp_tensor.GetThreadBuffer());
+                });
+            });
+        });
+
+        return c_block_tensor;
+    }
+#endif
 };
 
 } // namespace block
--- include/ck/tile_program/tile/load_tile.hpp
+++ include/ck/tile_program/tile/load_tile.hpp
@@ -19,6 +19,7 @@ namespace tile_program {
 // detail used by tile-programming APIs(), not supposed to be used directly
 namespace detail {
 
+#if 0
 // "Y dimension": Y dimensions inside TileWindowWithStaticDistribution
 // input:
 //   y_slice_origin: starting slice origin of Y dimension
@@ -157,6 +158,130 @@ __device__ auto load_sliced_thread_data_from_tile_window(
 
     return thread_buf;
 }
+#endif
+
+// "Y dimension": Y dimensions inside TileWindowWithStaticDistribution
+// input:
+//   tile_window
+// output:
+//   A StaticBuffer holding thread data, and data layout is hardcoded to be in the order of
+//   [Y0, Y1, Y2, ...]
+template <typename BottomTensorView_,
+          typename WindowLengths_,
+          typename TileDistribution_>
+__device__ auto load_thread_data_from_tile_window(
+    TileWindowWithStaticDistribution<BottomTensorView_, WindowLengths_, TileDistribution_>&
+        tile_window)
+{
+    using DataType         = remove_cvref_t<typename BottomTensorView_::DataType>;
+    using BottomTensorView = remove_cvref_t<BottomTensorView_>;
+    using WindowLengths    = remove_cvref_t<WindowLengths_>;
+    using TileDstr         = remove_cvref_t<TileDistribution_>;
+    using TileWindow = TileWindowWithStaticDistribution<BottomTensorView, WindowLengths, TileDstr>;
+
+    constexpr auto tile_dstr = TileDstr{};
+
+    constexpr auto thread_tensor_lengths_ys =
+        to_sequence(tile_dstr.GetYs2DDescriptor().GetLengths());
+
+    constexpr index_t NDimP = TileDstr::GetNumOfDimensionP();
+    constexpr index_t NDimY = TileDstr::GetNumOfDimensionY();
+
+    static_assert(TileWindow::HasStaticTileDistribution(),
+                  "wrong! assume static tile distribution");
+
+    constexpr index_t thread_element_size =
+        container_reduce(thread_tensor_lengths_ys, math::multiplies{}, 1);
+
+    StaticBuffer<AddressSpaceEnum::Vgpr, DataType, thread_element_size, true> thread_buf;
+
+    constexpr auto tmp = [&thread_tensor_lengths_ys]() {
+        const auto [ys_vector_lengths, ys_vector_strides] =
+            TileWindow::GetWindowAdaptorYsSafeVectorLengthStrides();
+
+        index_t VectorDimY      = 0;
+        index_t ScalarPerVector = 1;
+
+        for(index_t i = 0; i < NDimY; ++i)
+        {
+            if(ys_vector_strides[i] == 1 && ys_vector_lengths[i] > ScalarPerVector)
+            {
+                ScalarPerVector = ys_vector_lengths[i];
+                VectorDimY      = i;
+            }
+        }
+
+        return make_tuple(VectorDimY, ScalarPerVector);
+    }();
+
+    constexpr index_t VectorDimY      = tmp.template At<0>();
+    constexpr index_t ScalarPerVector = tmp.template At<1>();
+
+    // FIXME
+    using DimAccessOrder = typename arithmetic_sequence_gen<0, NDimY, 1>::type;
+
+    constexpr auto scalars_per_access_arr = generate_array(
+        [&](auto i) { return (i == VectorDimY) ? ScalarPerVector : 1; }, Number<NDimY>{});
+
+    constexpr auto scalars_per_access = TO_SEQUENCE(scalars_per_access_arr, NDimY);
+
+    using vector_type_t = vector_type_maker_t<DataType, ScalarPerVector>;
+    using vector_t      = typename vector_type_t::type;
+
+    using SFC_Ys =
+        SpaceFillingCurve<decltype(thread_tensor_lengths_ys), DimAccessOrder, decltype(scalars_per_access)>;
+
+    constexpr index_t num_access = SFC_Ys::GetNumOfAccess();
+
+    static_assert(num_access > 0, "wrong! num_access should be larger than 0");
+
+    // loop over thread tensor space [y0, y1, ...]
+    static_for<0, num_access, 1>{}([&](auto iAccess) {
+        // read from bottom tensor
+        const vector_t vec_value =
+            tile_window.GetBottomTensorView().template GetVectorizedElements<vector_t>(
+                tile_window.GetBottomTensorThreadCoordinate());
+
+        const vector_type_t vec{vec_value};
+
+        // data index [y0, y1, ...]
+        constexpr auto idx_ys_start = SFC_Ys::GetIndex(iAccess);
+
+        // write into distributed tensor
+        static_for<0, ScalarPerVector, 1>{}([&](auto j) {
+            constexpr auto idx_ys = generate_array(
+                [&](auto jj) {
+                    return jj == VectorDimY ? (idx_ys_start[jj] + j) : idx_ys_start[jj];
+                },
+                Number<NDimY>{});
+
+            constexpr index_t d = tile_dstr.GetYs2DDescriptor().CalculateOffset(idx_ys);
+
+            thread_buf.template At<d>() = vec.template AsType<DataType>()[j];
+        });
+
+        // move thread coordinate
+        if constexpr(iAccess.value != num_access - 1)
+        {
+            constexpr auto idx_diff_ys = SFC_Ys::GetForwardStep(iAccess);
+
+            constexpr auto idx_diff_ps_ys = container_concat(Array<index_t, NDimP>{0}, idx_diff_ys);
+
+            tile_window.MoveWindowAdaptorAndBottomTensorThreadCoordinate(idx_diff_ps_ys);
+        }
+    });
+
+    // move thread coordinate back to origin
+    {
+        constexpr auto idx_diff_ys = SFC_Ys::GetStepBetween(Number<num_access - 1>{}, Number<0>{});
+
+        constexpr auto idx_diff_ps_ys = container_concat(Array<index_t, NDimP>{0}, idx_diff_ys);
+
+        tile_window.MoveWindowAdaptorAndBottomTensorThreadCoordinate(idx_diff_ps_ys);
+    }
+
+    return thread_buf;
+}
 
 } // namespace detail
 
@@ -177,12 +302,10 @@ load_tile(TileWindowWithStaticDistribution<BottomTensorView_, WindowLengths_, Ti
 
     constexpr auto tile_dstr = TileDstr{};
 
-    constexpr index_t NDimY = tile_dstr.GetYs2DDescriptor().GetNumOfDimension();
-
     auto dstr_tensor = make_static_distributed_tensor<DataType>(tile_dstr);
 
-    dstr_tensor.GetThreadBuffer() = detail::load_sliced_thread_data_from_tile_window(
-        tile_window, MultiIndex<NDimY>{0}, to_sequence(tile_dstr.GetYs2DDescriptor().GetLengths()));
+    dstr_tensor.GetThreadBuffer() = detail::load_thread_data_from_tile_window(
+        tile_window);
 
     return dstr_tensor;
 }
