ebbafca21945e43ebe91f67b5abb109fb236b18f
Chao Liu <chao.liu2@amd.com>
2023-11-09 04:22:28 -0500

    fix build for old ck

--- library/include/ck/library/tensor_operation_instance/add_device_operation_instance.hpp
+++ library/include/ck/library/tensor_operation_instance/add_device_operation_instance.hpp
@@ -7,6 +7,7 @@
 #include <type_traits>
 
 #include "ck/utility/functional2.hpp"
+#include "ck/utility/remove_cvref.hpp"
 
 namespace ck {
 namespace tensor_operation {
@@ -20,7 +21,7 @@ void add_device_operation_instances(std::vector<std::unique_ptr<BaseOp>>& op_ins
     ck::static_for<0, std::tuple_size_v<NewOpInstances>, 1>{}([&](auto i) {
         const auto new_op_instance = std::get<i>(new_op_instances);
 
-        using NewOpInstance = std::remove_cvref_t<decltype(new_op_instance)>;
+        using NewOpInstance = remove_cvref_t<decltype(new_op_instance)>;
 
         static_assert(std::is_base_of_v<BaseOp, NewOpInstance>,
                       "wrong! NewOpInstance should be derived from BaseOp");
