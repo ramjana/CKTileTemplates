def015531d88b132d3b180ab33a574b720af3a24
Po-Yen, Chen <PoYen.Chen@amd.com>
2023-11-16 16:42:21 -0500

    Apply preliminary changes before merge

--- example/91_tile_program/fmha_fwd.cpp
+++ example/91_tile_program/fmha_fwd.cpp
@@ -1,4 +1,7 @@
+#include <array>
+#include <cstdlib>
 #include <cstring>
+#include <iomanip>
 #include <ostream>
 
 #include "ck/utility/common_header.hpp"
@@ -162,6 +165,18 @@ float invoker_fmha_kernel(const void* q_ptr,
     return ave_time;
 }
 
+std::array<ck::index_t, 4> get_lengths(bool permute,
+                                       ck::index_t b /*batch*/,
+                                       ck::index_t h /*nhead*/,
+                                       ck::index_t s /*seqlen*/,
+                                       ck::index_t d /*hdim*/)
+{
+    if(permute)
+        return {b, h, s, d};
+    else
+        return {b, s, h, d};
+}
+
 int main(int argc, char* argv[])
 {
     int do_validation    = 1;
@@ -199,26 +214,21 @@ int main(int argc, char* argv[])
     if(scale == .0f)
         scale = 1.0 / ck::math::sqrt(static_cast<float>(hdim_q)); // TODO: q ? v ?
 
-    auto get_lengths = [&](bool permute,
-                           ck::index_t b /*batch*/,
-                           ck::index_t h /*nhead*/,
-                           ck::index_t s /*seqlen*/,
-                           ck::index_t d /*hdim*/) {
-        if(permute)
-            return std::array<ck::index_t, 4>{b, h, s, d};
-        else
-            return std::array<ck::index_t, 4>{b, s, h, d};
-    };
-
     constexpr bool is_v_rowmajor =
         ck::is_same_v<typename FmhaKernelHDim64::VLayout, ck::tensor_layout::gemm::RowMajor>;
 
+    const auto q_host_lengths = get_lengths(i_perm, batch, nhead, seqlen_q, hdim_q);
+    const auto k_host_lengths = get_lengths(i_perm, batch, nhead, seqlen_k, hdim_q);
+    const auto v_host_lengths =
+        (is_v_rowmajor ? get_lengths(i_perm, batch, nhead, seqlen_k, hdim_v)
+                       : get_lengths(i_perm, batch, nhead, hdim_v, seqlen_k));
+    const auto o_host_lengths = get_lengths(o_perm, batch, nhead, seqlen_q, hdim_v);
+
     // host verify
-    Tensor<QDataType> q_host(get_lengths(i_perm, batch, nhead, seqlen_q, hdim_q));
-    Tensor<KDataType> k_host(get_lengths(i_perm, batch, nhead, seqlen_k, hdim_q));
-    Tensor<VDataType> v_host(is_v_rowmajor ? get_lengths(i_perm, batch, nhead, seqlen_k, hdim_v)
-                                           : get_lengths(i_perm, batch, nhead, hdim_v, seqlen_k));
-    Tensor<ODataType> o_host(get_lengths(o_perm, batch, nhead, seqlen_q, hdim_v));
+    Tensor<QDataType> q_host(q_host_lengths);
+    Tensor<KDataType> k_host(k_host_lengths);
+    Tensor<VDataType> v_host(v_host_lengths);
+    Tensor<ODataType> o_host(o_host_lengths);
 
 #if 0
     ck::utils::FillUniformDistributionIntegerValue<QDataType>{-2.f, 2.f}(q_host);
@@ -230,18 +240,19 @@ int main(int argc, char* argv[])
     ck::utils::FillUniformDistribution<VDataType>{-.5f, .5f}(v_host);
 #endif
 
-    DeviceMem q_buf(sizeof(QDataType) * q_host.GetElementSpaceSize());
-    DeviceMem k_buf(sizeof(KDataType) * k_host.GetElementSpaceSize());
-    DeviceMem v_buf(sizeof(VDataType) * v_host.GetElementSpaceSize());
-    DeviceMem o_buf(sizeof(ODataType) * o_host.GetElementSpaceSize());
+    DeviceMem q_buf(q_host.GetElementSpaceSizeInBytes());
+    DeviceMem k_buf(k_host.GetElementSpaceSizeInBytes());
+    DeviceMem v_buf(v_host.GetElementSpaceSizeInBytes());
+    DeviceMem o_buf(o_host.GetElementSpaceSizeInBytes());
 
-    q_buf.ToDevice(q_host.mData.data());
-    k_buf.ToDevice(k_host.mData.data());
-    v_buf.ToDevice(v_host.mData.data());
+    q_buf.ToDevice(q_host.data());
+    k_buf.ToDevice(k_host.data());
+    v_buf.ToDevice(v_host.data());
 
     std::cout << "batch:" << batch << ", nhead:" << nhead << ", seqlen_q:" << seqlen_q
               << ", seqlen_k:" << seqlen_k << ", hdim_q:" << hdim_q << ", hdim_v:" << hdim_v
-              << ", scale:" << scale << ", i_perm:" << i_perm << ", o_perm:" << o_perm
+              << ", scale:" << scale << ", i_perm:" << std::boolalpha << i_perm
+              << ", o_perm:" << std::boolalpha << o_perm
               << ", v:" << std::string(FmhaKernelHDim64::VLayout::name) << std::flush << std::endl;
 
     float ave_time = 0;
@@ -275,8 +286,8 @@ int main(int argc, char* argv[])
                                                           o_perm);
     else
     {
-        std::cout << "not support hdim, will not run" << std::endl;
-        return -1;
+        std::cerr << "not support hdim, will not run" << std::endl;
+        return EXIT_FAILURE;
     }
 
     std::size_t flop = std::size_t(2) * batch * nhead * seqlen_q * seqlen_k * hdim_q +
@@ -294,61 +305,60 @@ int main(int argc, char* argv[])
     std::cout << "Perf: " << ave_time << " ms, " << tflops << " TFlops, " << gb_per_sec << " GB/s"
               << std::endl;
 
-    if(do_validation)
+    if(!do_validation)
     {
-        Tensor<QDataType> q_host_ref({batch * nhead, seqlen_q, hdim_q});
-        Tensor<KDataType> k_host_ref({batch * nhead, seqlen_k, hdim_q});
-        const auto v_lengths = std::array<ck::index_t, 3>{batch * nhead, hdim_v, seqlen_k};
-        const auto v_strides = is_v_rowmajor
-                                   ? std::array<ck::index_t, 3>{hdim_v * seqlen_k, 1, hdim_v}
-                                   : std::array<ck::index_t, 3>{hdim_v * seqlen_k, seqlen_k, 1};
-        Tensor<VDataType> v_host_ref(v_lengths, v_strides);
-        Tensor<ODataType> o_host_ref({batch * nhead, seqlen_q, hdim_v});
-        Tensor<ODataType> o_host_result_ref(get_lengths(o_perm, batch, nhead, seqlen_q, hdim_v));
-
-        Tensor<SMPLComputeDataType> s_host_ref({batch * nhead, seqlen_q, seqlen_k});
-        Tensor<PDataType> p_host_ref({batch * nhead, seqlen_q, seqlen_k});
-
-        // clang-format off
-        // permute
-        if(i_perm) q_host.ForEach([&](auto& self, auto idx) { q_host_ref(idx[0] * nhead + idx[1], idx[2], idx[3]) = self(idx); });
-        else       q_host.ForEach([&](auto& self, auto idx) { q_host_ref(idx[0] * nhead + idx[2], idx[1], idx[3]) = self(idx); });
-
-        if(i_perm) k_host.ForEach([&](auto& self, auto idx) { k_host_ref(idx[0] * nhead + idx[1], idx[2], idx[3]) = self(idx); });
-        else       k_host.ForEach([&](auto& self, auto idx) { k_host_ref(idx[0] * nhead + idx[2], idx[1], idx[3]) = self(idx); });
-
-        if constexpr (is_v_rowmajor) {
-            //                              v_host ：b, h, s, d, v_host_ref : batch*hdim*seq
-            if(i_perm) v_host.ForEach([&](auto& self, auto idx) { v_host_ref(idx[0] * nhead + idx[1], idx[3], idx[2]) = self(idx); });
-            //                              v_host : b, s, h, d, v_host_ref : batch*hdim*seq
-            else       v_host.ForEach([&](auto& self, auto idx) { v_host_ref(idx[0] * nhead + idx[2], idx[3], idx[1]) = self(idx); });
-        }
-        else {
-            if(i_perm) v_host.ForEach([&](auto& self, auto idx) { v_host_ref(idx[0] * nhead + idx[1], idx[2], idx[3]) = self(idx); });
-            else       v_host.ForEach([&](auto& self, auto idx) { v_host_ref(idx[0] * nhead + idx[2], idx[1], idx[3]) = self(idx); });
-        }
-
-        // reference
-        reference_batched_gemm<QDataType, KDataType, SaccDataType, SMPLComputeDataType>(
-            q_host_ref, k_host_ref, s_host_ref,
-            [](const QDataType& x) { return x; },
-            [](const KDataType& x) { return x; },
-            [&scale](const SaccDataType& x) { return scale * x; });
-        reference_batched_softmax<SMPLComputeDataType, SMPLComputeDataType, PDataType>(s_host_ref,
-                                                                                       p_host_ref);
-        reference_batched_gemm<PDataType, VDataType, OaccDataType, ODataType>(
-            p_host_ref, v_host_ref, o_host_ref);
-
-        // permute
-        if(o_perm) o_host_result_ref.ForEach([&](auto& self, auto idx) { self(idx) = o_host_ref(idx[0] * nhead + idx[1], idx[2], idx[3]); });
-        else       o_host_result_ref.ForEach([&](auto& self, auto idx) { self(idx) = o_host_ref(idx[0] * nhead + idx[2], idx[1], idx[3]); });
-        // clang-format on
-
-        o_buf.FromDevice(o_host.mData.data());
-        return !ck::utils::check_err(o_host, o_host_result_ref);
+        return EXIT_SUCCESS;
     }
-    else
-    {
-        return 0;
+
+    o_buf.FromDevice(o_host.data());
+
+    Tensor<QDataType> q_host_ref({batch * nhead, seqlen_q, hdim_q});
+    Tensor<KDataType> k_host_ref({batch * nhead, seqlen_k, hdim_q});
+    const auto v_host_ref_lengths = std::array<ck::index_t, 3>{batch * nhead, hdim_v, seqlen_k};
+    const auto v_host_ref_strides =
+        is_v_rowmajor ? std::array<ck::index_t, 3>{hdim_v * seqlen_k, 1, hdim_v}
+                      : std::array<ck::index_t, 3>{hdim_v * seqlen_k, seqlen_k, 1};
+    Tensor<VDataType> v_host_ref(v_host_ref_lengths, v_host_ref_strides);
+    Tensor<ODataType> o_host_ref({batch * nhead, seqlen_q, hdim_v});
+    Tensor<ODataType> o_host_result_ref(get_lengths(o_perm, batch, nhead, seqlen_q, hdim_v));
+
+    Tensor<SMPLComputeDataType> s_host_ref({batch * nhead, seqlen_q, seqlen_k});
+    Tensor<PDataType> p_host_ref({batch * nhead, seqlen_q, seqlen_k});
+
+    // clang-format off
+    // permute
+    if(i_perm) q_host.ForEach([&](auto& self, auto idx) { q_host_ref(idx[0] * nhead + idx[1], idx[2], idx[3]) = self(idx); });
+    else       q_host.ForEach([&](auto& self, auto idx) { q_host_ref(idx[0] * nhead + idx[2], idx[1], idx[3]) = self(idx); });
+
+    if(i_perm) k_host.ForEach([&](auto& self, auto idx) { k_host_ref(idx[0] * nhead + idx[1], idx[2], idx[3]) = self(idx); });
+    else       k_host.ForEach([&](auto& self, auto idx) { k_host_ref(idx[0] * nhead + idx[2], idx[1], idx[3]) = self(idx); });
+
+    if constexpr (is_v_rowmajor) {
+        //                              v_host ：b, h, s, d, v_host_ref : batch*hdim*seq
+        if(i_perm) v_host.ForEach([&](auto& self, auto idx) { v_host_ref(idx[0] * nhead + idx[1], idx[3], idx[2]) = self(idx); });
+        //                              v_host : b, s, h, d, v_host_ref : batch*hdim*seq
+        else       v_host.ForEach([&](auto& self, auto idx) { v_host_ref(idx[0] * nhead + idx[2], idx[3], idx[1]) = self(idx); });
     }
+    else {
+        if(i_perm) v_host.ForEach([&](auto& self, auto idx) { v_host_ref(idx[0] * nhead + idx[1], idx[2], idx[3]) = self(idx); });
+        else       v_host.ForEach([&](auto& self, auto idx) { v_host_ref(idx[0] * nhead + idx[2], idx[1], idx[3]) = self(idx); });
+    }
+
+    // reference
+    reference_batched_gemm<QDataType, KDataType, SaccDataType, SMPLComputeDataType>(
+        q_host_ref, k_host_ref, s_host_ref,
+        [](const QDataType& x) { return x; },
+        [](const KDataType& x) { return x; },
+        [&scale](const SaccDataType& x) { return scale * x; });
+    reference_batched_softmax<SMPLComputeDataType, SMPLComputeDataType, PDataType>(s_host_ref,
+                                                                                    p_host_ref);
+    reference_batched_gemm<PDataType, VDataType, OaccDataType, ODataType>(
+        p_host_ref, v_host_ref, o_host_ref);
+
+    // permute
+    if(o_perm) o_host_result_ref.ForEach([&](auto& self, auto idx) { self(idx) = o_host_ref(idx[0] * nhead + idx[1], idx[2], idx[3]); });
+    else       o_host_result_ref.ForEach([&](auto& self, auto idx) { self(idx) = o_host_ref(idx[0] * nhead + idx[2], idx[1], idx[3]); });
+    // clang-format on
+
+    return !ck::utils::check_err(o_host, o_host_result_ref);
 }
