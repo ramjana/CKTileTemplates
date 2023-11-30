100161279355058afa623dbb4559ca24f3128eb7
Po-Yen, Chen <PoYen.Chen@amd.com>
2023-11-30 14:29:14 -0500

    Refine reference_batched_masking()

--- example/91_tile_program/reference_batched_masking.hpp
+++ example/91_tile_program/reference_batched_masking.hpp
@@ -7,7 +7,6 @@
 #include "ck/library/utility/host_tensor.hpp"
 #include "ck/tile_program/block_tile/block_masking_specialization.hpp"
 
-
 template <typename CDataType, typename MaskingType>
 void reference_batched_masking(Tensor<CDataType>& c_b_m_n)
 {
@@ -16,17 +15,26 @@ void reference_batched_masking(Tensor<CDataType>& c_b_m_n)
 
     const int MNDiff = M - N;
 
-    auto f = [&](auto batch){
-        for(int n = 0; n < N; ++n){
-            for(int m = 0; m < M; ++m){
-                if(std::is_same_v<MaskingType, ck::tile_program::block::MaskDisabledPredicate>){}
-                if(std::is_same_v<MaskingType, ck::tile_program::block::MaskUpperTriangleFromTopLeftPredicate>){
-                    if(n > m){
+    auto f = [&](auto batch) {
+        for(int n = 0; n < N; ++n)
+        {
+            for(int m = 0; m < M; ++m)
+            {
+                if constexpr(std::is_same_v<
+                                 MaskingType,
+                                 ck::tile_program::block::MaskUpperTriangleFromTopLeftPredicate>)
+                {
+                    if(n > m)
+                    {
                         c_b_m_n(batch, m, n) = -ck::NumericLimits<CDataType>::Infinity();
                     }
                 }
-                if(std::is_same_v<MaskingType, ck::tile_program::block::MaskUpperTriangleFromBottomRightPredicate>){
-                    if(n > m - MNDiff){
+                else if constexpr(std::is_same_v<MaskingType,
+                                                 ck::tile_program::block::
+                                                     MaskUpperTriangleFromBottomRightPredicate>)
+                {
+                    if(n > m - MNDiff)
+                    {
                         c_b_m_n(batch, m, n) = -ck::NumericLimits<CDataType>::Infinity();
                     }
                 }
@@ -34,6 +42,6 @@ void reference_batched_masking(Tensor<CDataType>& c_b_m_n)
         }
     };
 
-    make_ParallelTensorFunctor(f, c_b_m_n.mDesc.GetLengths()[0])(
-        std::thread::hardware_concurrency());
+    make_ParallelTensorFunctor(f,
+                               c_b_m_n.mDesc.GetLengths()[0])(std::thread::hardware_concurrency());
 }
