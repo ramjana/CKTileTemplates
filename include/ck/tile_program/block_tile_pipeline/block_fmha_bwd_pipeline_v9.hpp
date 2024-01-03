95008e9c31a7fac792c5c1c10013b0252c9f47d2
danyao12 <danyao12@amd.com>
2024-01-03 15:40:13 +0800

    continue update pipelinev9

--- include/ck/tile_program/block_tile_pipeline/block_fmha_bwd_pipeline_v9.hpp
+++ include/ck/tile_program/block_tile_pipeline/block_fmha_bwd_pipeline_v9.hpp
@@ -315,7 +315,7 @@ struct BlockFmhaBwdPipelineV9
 
             if constexpr(k0_loops > 2)
             {
-                static_for<0, k0_loops - 1, 1>{}([&](auto i_k0) {
+                static_for<0, k0_loops - 2, 1>{}([&](auto i_k0) {
                     block_sync_lds();
                     gemm_0(st_acc,
                            q_lds_window,
